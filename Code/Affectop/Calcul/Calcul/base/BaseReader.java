01f8a1b6064662a3aea04fae1289e0bc4ac1ec3c
root <root@Pc-valentin.localdomain>
2018-12-08 03:48:55 +0100

    update

--- Code/Affectop/Calcul/Calcul/base/BaseReader.java
+++ Code/Affectop/Calcul/Calcul/base/BaseReader.java
@@ -2,11 +2,14 @@ package Calcul.base;
 
 import java.sql.*;
 
+import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
+
 /**
  * This base reader can only be used with a mysql database
  * 
  */
 public class BaseReader implements BaseAdapter {
+	private Connection conn;
 
 	public BaseReader() {
 
@@ -19,50 +22,30 @@ public class BaseReader implements BaseAdapter {
 		try {
 			// create our mysql database connection
 			String myDriver = "com.mysql.jdbc.Driver";
-			
+
 			String myUrl = "jdbc:mysql://51.75.120.5/affectop_test";
 			System.out.println(myUrl);
 			Class.forName(myDriver);
-			Connection conn = DriverManager.getConnection(myUrl, "baseaccess", "affectop2018");
-			System.out.println(conn);
-			// our SQL SELECT query.
-			// if you only need a few columns, specify them by name instead of using "*"
-			String query = "SELECT * FROM Students";
+			System.out.println("test");
+			conn = DriverManager.getConnection(myUrl, "basaccess", "affectop2018");
 
-			// create the java statement
-			Statement st = conn.createStatement();
-
-			// execute the query, and get a java resultset
-			ResultSet rs = st.executeQuery(query);
-
-			// iterate through the java resultset
-			System.out.println("Affichage :");
-			while (rs.next()) {
-				String token = rs.getString("token");
-				String firstName = rs.getString("firstName");
-				String lastName = rs.getString("lastName");
-				String numetu = rs.getString("numEtudiant");
-				String mail = rs.getString("mail");
-				String step = rs.getString("step");
-				String year = rs.getString("year");
-				// print the results
-				System.out.format("%s, %s, %s, %s, %s, %s, %s\n", firstName, lastName,numetu,mail,token,step,year);
-			}
-			st.close();
+			System.out.println("connexion etablie");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	
 	/**
 	 * Return the database content associated with the tag
-	 * @param tag 
-	 * @param token primary key used for accessing database specified value for a given student
-	 * */
+	 * 
+	 * @param tag
+	 * @param token
+	 *            primary key used for accessing database specified value for a
+	 *            given student
+	 */
 	public String tagRequest(String tag, String token) {
-		String tagcontent=tag.substring(1, tag.length()-1);
+		String tagcontent = tag.substring(1, tag.length() - 1);
 		System.out.println(tagcontent);
 		if (tagcontent.equalsIgnoreCase("nom")) {
 			return nameRequest(token);
@@ -79,14 +62,97 @@ public class BaseReader implements BaseAdapter {
 		return "/!\\unknown tag/!\\ ";
 	}
 
+	private String studentsQueryBuilder(String col, String token) {
+
+		return "SELECT " + col + " FROM Students WHERE token=\"" + token + "\";";
+	}
+
+	private String studentsQueryRequest(String col, String token) {
+
+		String query = studentsQueryBuilder(col, token);
+		System.out.println(query);
+		Statement st;
+		String res = "";
+		try {
+			st = conn.createStatement();
+
+			// execute the query, and get a java resultset
+			ResultSet rs = st.executeQuery(query);
+
+			// iterate through the java resultset
+
+			while (rs.next()) {
+
+				res = rs.getString(col);
+			}
+			st.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return res;
+
+	}
+
+	private String teachersQueryBuilder(String col, String token) {
+		return "SELECT " + col + " FROM Teachers WHERE token=\"" + token + "\";";
+
+	}
+
+	private String teachersQueryRequest(String col, String token) {
+
+		String query = teachersQueryBuilder(col, token);
+		System.out.println(query);
+		Statement st;
+		String res = "";
+		try {
+			st = conn.createStatement();
+
+			// execute the query, and get a java resultset
+			ResultSet rs = st.executeQuery(query);
+
+			// iterate through the java resultset
+
+			while (rs.next()) {
+
+				res = rs.getString(col);
+			}
+			st.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return res;
+
+	}
+
 	public String nameRequest(String token) {
-		return "nameRequest";
+		return studentsQueryRequest("lastname", token);
+
 	}
 
 	public String firstNameRequest(String token) {
 
-		return "firstNameRequest";
+		return studentsQueryRequest("firstname", token);
 	}
+
+	public int tokenIdentification(String token) {
+
+		String resQueryStudent = studentsQueryRequest("token", token);
+		if (resQueryStudent != null && !resQueryStudent.equals(""))
+			return 1;
+
+		String resQueryTeacher = teachersQueryRequest("token", token);
+
+		if (resQueryTeacher != null && !resQueryTeacher.equals(""))
+			return 2;
+		return 0;
+	}
+	
 
 	private String affectListRequest(String token) {
 
