ab607343ec3dbaf7645048e400ed54f96ce46566
VALLETMat <43214221+VALLETMat@users.noreply.github.com>
2018-12-11 21:33:42 +0100

    Add files via upload
    baseUpdate

--- Code/Affectop/Calcul/Calcul/base/BaseReader.java
+++ Code/Affectop/Calcul/Calcul/base/BaseReader.java
@@ -1,19 +1,28 @@
-package Calcul.base;
+package baseIO;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
-import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
+import calcul.Affectop;
+import calcul.Option;
+import calcul.Student;
 
 /**
  * This base reader can only be used with a mysql database
  * 
  */
-public class BaseReader implements BaseAdapter {
-	private Connection conn;
+public class BaseReader extends BaseHandler{
 
-	public BaseReader() {
-
+	Map<Integer,Option> options = new HashMap<Integer, Option>();
+	Map<Integer,Student> students = new HashMap<Integer, Student>();
+	Statement st;
+	
+	
+	
+	ResultSet getResultOfQuery(String query) {
 		/**
 		 * A Java MySQL SELECT statement example. Demonstrates the use of a SQL SELECT
 		 * statement against a MySQL database, called from a Java program.
@@ -23,176 +32,238 @@ public class BaseReader implements BaseAdapter {
 		try {
 			// create our mysql database connection
 			String myDriver = "com.mysql.jdbc.Driver";
-
+			
 			String myUrl = "jdbc:mysql://51.75.120.5/affectop_test";
-			System.out.println(myUrl);
+			
 			Class.forName(myDriver);
-			System.out.println("test");
-			conn = DriverManager.getConnection(myUrl, "basaccess", "affectop2018");
-
-			System.out.println("connexion etablie");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	/**
-	 * Return the database content associated with the tag
-	 * 
-	 * @param tag
-	 * @param token
-	 *            primary key used for accessing database specified value for a
-	 *            given student
-	 */
-	public String tagRequest(String tag, String token) {
-		String tagcontent = tag.substring(1, tag.length() - 1);
-		System.out.println(tagcontent);
-		if (tagcontent.equalsIgnoreCase("nom")) {
-			return nameRequest(token);
-		}
-		if (tagcontent.equalsIgnoreCase("prenom")) {
-			return firstNameRequest(token);
-		}
-		if (tagcontent.equalsIgnoreCase("LISTE_AFFECTATION")) {
-			return affectListRequest(token);
-		}
-		if (tagcontent.equalsIgnoreCase("date")) {
-			return dateRequest(token);
-		}
-		return "/!\\unknown tag/!\\ ";
-	}
-
-	private String studentsQueryBuilder(String col, String token) {
-
-		return "SELECT " + col + " FROM Students WHERE token=\"" + token + "\";";
-	}
-
-	private String studentsQueryRequest(String col, String token) {
-
-		String query = studentsQueryBuilder(col, token);
-		System.out.println(query);
-		Statement st;
-		String res = "";
-		try {
+			Connection conn = DriverManager.getConnection(myUrl, "baseaccess", "affectop2018");
+			
 			st = conn.createStatement();
 
-			// execute the query, and get a java resultset
-			ResultSet rs = st.executeQuery(query);
+			ResultSet rs = st.executeQuery(query);	
+			return rs;
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public ArrayList<ArrayList<Option>> getOptionsPerDays(int year){
+		String query = "SELECT * FROM Options where year = "+year+" ;";
 
-			// iterate through the java resultset
-
+		ResultSet rs = getResultOfQuery(query);		
+		ArrayList<ArrayList<Option>> result = new ArrayList<>();
+		
+		int nbDays = getNbDays(year);
+		for(int d = 0 ; d < nbDays ; d++) {
+			result.add(new ArrayList<>());
+		}
+		
+		try {
 			while (rs.next()) {
-
-				res = rs.getString(col);
+				String intitule = rs.getString("intitule");
+				
+				int size = rs.getInt("size");
+				int optionGroup = rs.getInt("optionGroup");
+				int id = rs.getInt("id");
+				
+				Option o = new Option(size, intitule, optionGroup,id);
+				options.put(id,o);
+				result.get(optionGroup-1).add(o);
+				//System.out.format("%s, %s, %s, %s, %s, %s, %s\n", firstName, lastName,numetu,mail,token,step,year);
 			}
-			st.close();
+			return result;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return result;
+		}
+	}
+	
+	
+	public ArrayList<Student> getStudents(int year){
+		String query = "SELECT * FROM Students where year = "+year+" ;";
+		ResultSet rs = getResultOfQuery(query);
+		
+		ArrayList<Student> result = new ArrayList<>();
+		
+		int nbDays = getNbDays(year);
+		// iterate through the java resultset
+		try {
+			while (rs.next()) {
+				
+				String mail = rs.getString("mail");
+				Integer numEtu = rs.getInt("numEtudiant");
+				
+				System.out.println(nbDays+" "+ numEtu+" "+ year);
+				
+				ArrayList<LinkedList<Option>> preferences = getStudentPreference(nbDays, numEtu, year);
+				Student s =new Student(mail, preferences, nbDays,numEtu); 
+				students.put(numEtu,s);
+				result.add(s);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return result;
+		}
+		return result;
+	}
+	
+	
+	public ArrayList<Option> getOptions(int year){
+		String query = "SELECT * FROM Options where year = "+year+" ;";
 
+		ResultSet rs = getResultOfQuery(query);		
+		ArrayList<Option> result = new ArrayList<>();
+		
+		options.clear();
+		
+		try {
+			while (rs.next()) {
+				String intitule = rs.getString("intitule");
+				
+				int size = rs.getInt("size");
+				int optionGroup = rs.getInt("optionGroup");
+				int id = rs.getInt("id");
+				
+				Option o = new Option(size, intitule, optionGroup,id);
+				options.put(id,o);
+				result.add(o);
+				//System.out.format("%s, %s, %s, %s, %s, %s, %s\n", firstName, lastName,numetu,mail,token,step,year);
+			}
+			return result;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return result;
+		}
+	}
+
+	
+	public ArrayList<LinkedList<Option>> getStudentPreference(int nbDays, int studentID, int year){
+			ArrayList<LinkedList<Option>> result = new ArrayList<>();
+			String query = 
+				"SELECT * FROM Preferences where numEtudiant = '"+studentID+"' ORDER BY choice;" ;
+			ResultSet rs = getResultOfQuery(query);
+			ArrayList<Option> optionsAllDaysSortedByPreferences = new ArrayList<>();
+			try {
+				while (rs.next()) {
+					optionsAllDaysSortedByPreferences.add(options.get(rs.getInt("optionId")));
+				}
+				//System.out.println(optionsAllDaysSortedByPreferences);
+				for(int d = 1 ; d <= nbDays ; d ++) {
+					LinkedList<Option> todayPreference = new LinkedList<>() ;
+					for (Option opt : optionsAllDaysSortedByPreferences) {
+						System.out.println(opt);
+						if(opt.day == d)
+							todayPreference.addLast(opt);
+					}
+					result.add(todayPreference);
+				}				
+				
+				return result;
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				return result;
+			}
+	}
+		
+	public int getNbDays(int year) {
+		String query = 
+				"select MAX(optionGroup) from Options where year ="+year+";" ;
+		ResultSet rs = getResultOfQuery(query);
+		
+		try {
+			if(rs.next())
+				return  rs.getInt("MAX(optionGroup)");
+		}
+		catch(Exception e) {
+			System.out.println("ERROR");
+			e.printStackTrace();
+		}
+		return 1;
+	}
+	
+
+	public Map<Option,ArrayList<Option>> getIncompatibilities(){
+		Map<Option,ArrayList<Option>> incompatibilities = new HashMap<Option, ArrayList<Option>>();
+		
+		for(Option opt1 : options.values()) {
+			incompatibilities.put(opt1,new ArrayList<>());
+			for(Option opt2 : options.values()) {
+			if(opt1.intitule == opt2.intitule)
+				incompatibilities.get(opt1).add(opt2);
+			}
+		}
+		return incompatibilities;
+	}
+	
+	public Map<Student, ArrayList<Option>> getRepeater(int year) {
+		String query = "SELECT * FROM Repeaters WHERE optionId IN ("
+				+ "SELECT id FROM Options WHERE year ="+year+") ;";
+		ResultSet rs = getResultOfQuery(query);
+		
+		Map<Student, ArrayList<Option>> repeaters = new HashMap<Student, ArrayList<Option>>();
+		
+
+		// iterate through the java resultset
+		try {
+			while (rs.next()) {
+				
+				int opt = rs.getInt("optionId");
+				int numEtu = rs.getInt("numEtudiant");
+
+				if(!repeaters.containsKey(students.get(numEtu)))
+					repeaters.put(students.get(numEtu), new ArrayList<>());
+				System.out.println();
+				System.out.println(students.get(numEtu));
+				System.out.println(numEtu);
+				System.out.println(students);
+				System.out.println();
+				repeaters.get(students.get(numEtu)).add(options.get(opt));
+				//System.out.format("%s, %s, %s, %s, %s, %s, %s\n", firstName, lastName,numetu,mail,token,step,year);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return repeaters;
+		}
+		return repeaters;
+	}
+	
+
+	public static void main(String[] args) {
+		BaseReader br = new BaseReader();
+		int nbDays = br.getNbDays(2017);
+		ArrayList<ArrayList<Option>> options = br.getOptionsPerDays(2017);
+		ArrayList<Student> students = br.getStudents(2017);
+		Map<Student, ArrayList<Option>> repeaters = br.getRepeater(2017);
+		Map<Option,ArrayList<Option>> incompatibilities = br.getIncompatibilities();
+		
+		
+		System.out.println(students);
+		ArrayList<Student> scatterBrainStudents = new ArrayList<>();
+		for(Student s : students) {
+			if(s.preferences.get(0).isEmpty())
+				scatterBrainStudents.add(s);
+		}
+		
+		students.removeAll(scatterBrainStudents);
+		System.out.println(br.getOptions(2017));
+
+
+		try {
+			if(br != null && br.st != null)
+				br.st.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		return res;
-
-	}
-
-	private String teachersQueryBuilder(String col, String token) {
-		return "SELECT " + col + " FROM Teachers WHERE token=\"" + token + "\";";
-
-	}
-
-	private String teachersQueryRequest(String col, String token) {
-
-		String query = teachersQueryBuilder(col, token);
-		System.out.println(query);
-		Statement st;
-		String res = "";
-		try {
-			st = conn.createStatement();
-
-			// execute the query, and get a java resultset
-			ResultSet rs = st.executeQuery(query);
-
-			// iterate through the java resultset
-
-			while (rs.next()) {
-
-				res = rs.getString(col);
-			}
-			st.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return res;
-
-	}
-
-	public String nameRequest(String token) {
-		return studentsQueryRequest("lastname", token);
-
-	}
-
-	public String firstNameRequest(String token) {
-
-		return studentsQueryRequest("firstname", token);
-	}
-
-	public int tokenIdentification(String token) {
-
-		String resQueryStudent = studentsQueryRequest("token", token);
-		if (resQueryStudent != null && !resQueryStudent.equals(""))
-			return 1;
-
-		String resQueryTeacher = teachersQueryRequest("token", token);
-
-		if (resQueryTeacher != null && !resQueryTeacher.equals(""))
-			return 2;
-		return 0;
-	}
-
-	//TODO ne marche pas car on va confondre les années et les sessions, il faut une jointure entre le prof et les options puis avec les prefs et leseleves
-	public ArrayList<String> studentsValidateListRequest() {
-		ArrayList<String> students = new ArrayList<>();
-
-		String query = "SELECT token FROM Students WHERE numEtudiant in (SELECT DISTINCT numEtudiant FROM  Preferences );";
-	//	String query = "SELECT * FROM Students INNER JOIN Teachers ON table1.id = table2.fk_id ;";
-		Statement st;
-		String res = "";
-		try {
-			st = conn.createStatement();
-
-			// execute the query, and get a java resultset
-			ResultSet rs = st.executeQuery(query);
-
-			// iterate through the java resultset
-
-			while (rs.next()) {
-
-				res = rs.getString("token");
-				students.add(res);
-			}
-			st.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return students;
-
-	}
-
-	private String affectListRequest(String token) {
-
-		return "affectListRequest";
-	}
-
-	public String dateRequest(String token) {
-
-		return "dateRequest";
+		
+		Affectop aff = new Affectop(nbDays, students, options, repeaters, scatterBrainStudents, incompatibilities);
+		System.out.println(aff.affectTop(nbDays,1));
+		
+		System.out.println(new calcul.Result(students).results);
 	}
 }
--- Code/Affectop/Calcul/Calcul/base/BaseWriter.java
+++ Code/Affectop/Calcul/Calcul/base/BaseWriter.java
@@ -2,14 +2,15 @@ package baseIO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
 import calcul.Option;
 import calcul.Result;
 import calcul.Result.Affectation;
+import calcul.Student;
 
 public class BaseWriter extends BaseHandler{
 	Statement st ;
@@ -33,7 +34,7 @@ public class BaseWriter extends BaseHandler{
 			st = conn.createStatement();
 		}
 		catch (Exception e) {
-		
+			e.printStackTrace();
 		}
 	}
 
@@ -82,6 +83,45 @@ public class BaseWriter extends BaseHandler{
 		}
 	}
 	
+	public void writePreference(int numEtudiant, ArrayList<Option> preferences) {
+		int index = 1;
+		StringBuilder query = new StringBuilder();
+		query.append("INSERT INTO Preferences (choice,optionId,numEtudiant) \nVALUES\n");
+		for(Option opt : preferences) {
+			query.append("("+index+","+opt.id+","+numEtudiant+"),");
+			index ++;
+		}
+		query.replace(query.length()-1, query.length(), ";");
+		
+		try {
+			System.out.println(query);
+			st.executeUpdate(query.toString());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	public void writeOptions(ArrayList<Option> options, int year) {
+		StringBuilder query = new StringBuilder();
+		query.append("INSERT INTO Options (intitule,description,size,optionGroup,year) \nVALUES\n");
+		for(Option opt : options) {
+			query.append("('"+opt.intitule+"','"+opt.description+"',"+opt.size+","+opt.day+","+year+"),");
+		}
+		query.replace(query.length()-1, query.length(), ";");
+		
+		try {
+			System.out.println(query);
+			st.executeUpdate(query.toString());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public void deleteStudent(String numEtudiant) {
 		String query = "DELETE FROM Students WHERE numEtudiant ="+numEtudiant+";";
 				
@@ -94,13 +134,66 @@ public class BaseWriter extends BaseHandler{
 		}
 	}
 	
+	public void writeRepeaters(Map<Student,ArrayList<Option>> repeaters, int year) {
+		StringBuilder query = new StringBuilder();
+		query.append("INSERT INTO Repeaters (numEtudiant,optionId,year)\nVALUES\n");
+		
+		for(Student s : repeaters.keySet()) {
+			for(Option opt : repeaters.get(s))
+				query.append("("+s.numEtu+","+opt.id+","+year+"),");
+		}
+		query.replace(query.length()-1, query.length(), ";");
+		try {
+			System.out.println(query);
+			st.executeUpdate(query.toString());
+		}		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		BaseWriter b = new BaseWriter();
 		b.initConnection();
-		b.writeStudent(1,"Jesus","Christ","1","JEEZ_eden.com","aaaaaaaaaaaaaaaa","1");
-		b.deleteStudent("1");
+		
+		//STUDENTS
+		//b.writeStudent(1,"Jesus","Christ","1","JEEZ_eden.com","aaaaaaaaaaaaaaaa","1");
+		//b.deleteStudent("1");
+		
+		//PREFERENCES
+		/*ArrayList<LinkedList<Option>> prefs = new ArrayList<>();
+		LinkedList<Option> opt = new LinkedList<Option>();
+		opt.add(new Option(1, "faire ses lacets", 1, 666));
+		opt.add(new Option(2, "karate", 2, 333));
+		prefs.add(opt);*/
+		
+		//b.writePreference(1, new ArrayList<>(opt));
+		
+		//OPTIONS
+		/*ArrayList<Option> options = new ArrayList<>();
+		
+		Option opt1 = new Option(10000, "blablabla", "RSA", 1, 99);
+		Option opt2 = new Option(10001, "spé chomage ", "arts du cirque ", 2, 13);
+		options.add(opt1);
+		options.add(opt2);*/
+		//b.writeOptions(options, 2017);
+		
+		//REPEATEARS
+		Map<Student, ArrayList<Option>> repeat = new HashMap<>();
+		
+		//b.writeStudent(1,"Jesus","Christ","1","JEEZ_eden.com","aaaaaaaaaaaaaaaa","1");
+		//b.writeStudent(1,"Monkey","D Luffy","2","gumm_gumm.com","wooon","2");
+		
+		BaseReader br = new BaseReader();
+		ArrayList<Option> ops = br.getOptions(2017);
+		ArrayList<Student> sts = br.getStudents(2017);
+		
+		
+		ArrayList<Option> jesusRepeater = new ArrayList<>();
+		jesusRepeater.add(ops.get(0));
+		repeat.put(sts.get(0), jesusRepeater);
+		
+		b.writeRepeaters(repeat,1997);
 		b.endConnection();
 	}
-	
 }
