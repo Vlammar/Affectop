93e47f75245879e6ae206f1a68537899c658dc0c
VALLETMat <43214221+VALLETMat@users.noreply.github.com>
2018-12-11 14:40:27 +0100

    Add files via upload
    BaseHandler

---
+++ Code/Affectop/Calcul/Calcul/base/BaseHandler.java
@@ -0,0 +1,36 @@
+package baseIO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class BaseHandler {
+	Statement st;
+	ResultSet getResultOfQuery(String query) {
+		/**
+		 * A Java MySQL SELECT statement example. Demonstrates the use of a SQL SELECT
+		 * statement against a MySQL database, called from a Java program.
+		 * 
+		 * Created by Alvin Alexander, http://alvinalexander.com
+		 */
+		try {
+			// create our mysql database connection
+			String myDriver = "com.mysql.jdbc.Driver";
+			
+			String myUrl = "jdbc:mysql://51.75.120.5/affectop_test";
+			
+			Class.forName(myDriver);
+			Connection conn = DriverManager.getConnection(myUrl, "baseaccess", "affectop2018");
+			
+			st = conn.createStatement();
+
+			ResultSet rs = st.executeQuery(query);	
+			return rs;
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
